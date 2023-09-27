38ff4fd4c0bfe312cc1b6a80cb8f95b470e10c26
abdulmuhyiismail <114898486+abdulmuhyiismail@users.noreply.github.com>
2023-09-27 12:29:45 +0700

    Add files via upload

---
+++ TugasOne.js
@@ -0,0 +1,24 @@
+// File: TugaOne.js
+
+// TugasOne
+const nama = "ami";
+const nim = "20220040045";
+
+// Operasi Aritmatika
+const angka1 = 10;
+const angka2 = 5;
+
+const tambah = angka1 + angka2;
+const kurang = angka1 - angka2;
+const kali = angka1 * angka2;
+const bagi = angka1 / angka2;
+
+// Menampilkan TugasOne
+console.log("Nama:", nama);
+console.log("NIM:", nim);
+
+// Menampilkan Hasil Operasi Aritmatika
+console.log("Hasil Penjumlahan:", tambah);
+console.log("Hasil Pengurangan:", kurang);
+console.log("Hasil perkalian:", kali);
+console.log("Hasil Pembagian:", bagi); 
